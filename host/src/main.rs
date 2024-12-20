9c3dc8473af2c65ab73cbfe974e49fafd7813cce
woojucy <woojucy2@gmail.com>
2024-12-20 12:14:22 +0900

    Modulize main code

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,56 +1,70 @@
 use methods::{RANGE_PROOF_ELF, RANGE_PROOF_ID};
 use num_bigint::BigUint;
-use risc0_zkvm::{default_prover, ExecutorEnv};
+use risc0_zkvm::{default_prover, ExecutorEnv, Receipt, ProveInfo};
 use std::str::FromStr;
 use std::time::Instant;
 use risc0_zkvm::serde::to_vec;
 
-pub const PRIME_P: &str = "8155133734070055735139271277173718200941522166153710213522626777763679009805792017274916613411023848268056376687809186180768200590914945958831360737612803";
-pub const PRIME_Q: &str = "13379153270147861840625872456862185586039997603014979833900847304743997773803109864546170215161716700184487787472783869920830925415022501258643369350348243";
-pub const GENERATOR: &str = "4";
+pub const MODULUS: &str = "8155133734070055735139271277173718200941522166153710213522626777763679009805792017274916613411023848268056376687809186180768200590914945958831360737612803";
+pub const BASE: &str = "4";
+pub const RANGE: &str = "4";
+pub const EXPONENT: &str = "4";
 
 fn main() {
-    let p = BigUint::from_str(PRIME_P).expect("Invalid number for P");
-    let q = BigUint::from_str(PRIME_Q).expect("Invalid number for Q");
-    let generator = BigUint::from_str(GENERATOR).expect("Invalid number for Generator");
+    let (base, modulus, range, exponent) = setup_inputs();
+    
+    let env = setup_env(&base, &modulus, &range, &exponent);
 
-    let modulus = p * q;
+    // Generate proof and get receipt
+    let receipt = generate_proof(env).receipt;
 
-    let range: BigUint = BigUint::parse_bytes(b"5000", 10).unwrap();
-    let exp: BigUint = BigUint::parse_bytes(b"5000", 10).unwrap();
+    // Verify the proof
+    verify_proof(&receipt);
+}
 
-    // Start timing for proof generation
-    let proof_start = Instant::now();
+fn setup_inputs() -> (BigUint, BigUint, BigUint, BigUint) {
+    let base = BigUint::from_str(BASE).expect("Invalid number for Base");
+    let modulus = BigUint::from_str(MODULUS).expect("Invalid number for Modulus");
+    let range = BigUint::from_str(RANGE).expect("Invalid number for Range");
+    let exponent = BigUint::from_str(EXPONENT).expect("Invalid number for Exponent");
+    (base, modulus,range, exponent)
+}
 
-    // let input_data = bincode::serialize(&(&generator, &modulus, &range, &exp)).unwrap();
-    let env = ExecutorEnv::builder()
-        .write(&to_vec(&generator.clone()).unwrap())
+fn setup_env<'a>(
+    base: &'a BigUint, 
+    modulus: &'a BigUint, 
+    range: &'a BigUint, 
+    exponent: &'a BigUint
+) -> ExecutorEnv<'a> {
+    ExecutorEnv::builder()
+        .write(&to_vec(base).unwrap())
         .unwrap()
-        .write(&to_vec(&modulus.clone()).unwrap())
+        .write(&to_vec(modulus).unwrap())
         .unwrap()
-        .write(&to_vec(&range.clone()).unwrap())
+        .write(&to_vec(range).unwrap())
         .unwrap()
-        .write(&to_vec(&exp.clone()).unwrap())
+        .write(&to_vec(exponent).unwrap())
         .unwrap()
         .build()
-        .unwrap();
-    //     .write_slice(&input_data)
-    //     .build()
-    //     .unwrap();
+        .unwrap()
+}
 
-    let prover = default_prover().prove(env, RANGE_PROOF_ELF).unwrap();
-    let receipt = prover.receipt;
-
+fn generate_proof(env: ExecutorEnv) -> ProveInfo {
+    println!("Starting proof generation...");
+    let proof_start = Instant::now();
+    let prove_info = default_prover().prove(env, RANGE_PROOF_ELF).unwrap();
     let proof_duration = proof_start.elapsed();
     println!("Proof generation completed in {:?}", proof_duration);
+    prove_info
+}
 
-    // Start timing for verification
+fn verify_proof(receipt: &Receipt) {
+    println!("Starting verification...");
     let verify_start = Instant::now();
 
     receipt.verify(RANGE_PROOF_ID).unwrap();
 
     let verify_duration = verify_start.elapsed();
-
     println!("Verification completed in {:?}", verify_duration);
 
     println!("Verified");
